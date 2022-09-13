319b830b9b0653f83f4ba4bce2ef6ee6d0c22419
gagnonal <gagnonal@educ.college-montreal.qc.ca>
2022-09-13 09:00:21 -0400

    Tp1

---
+++ venv/Lib/tp#1.py
@@ -0,0 +1,8 @@
+#je demande a l'utilisateur decrire une phrase
+test_string = input("Ecrivez une phrase")
+#je demande au programe de recrire la phrase
+print("la phrase original : " + test_string)
+#je demande au programe de conter le nombre de mots
+res = len(test_string.split())
+#je demande au programe de dire le nombre de mots
+print("le nombre de mots est :" + str(res))
